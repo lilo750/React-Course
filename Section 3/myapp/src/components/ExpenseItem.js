26a016c21374d021ea7955adde3b2b7e21a093b2
lilo <lilo750@yahoo.com>
2023-06-04 18:52:12 +0200

    created a component folder and a expense component inside it with simple strucure

---
+++ Section 3/myapp/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function Expense() {
+	return (
+		<div className='expense--component'>
+			<div>June, 04, 2023</div>
+			<h2>car insurauce</h2>
+			<div>$544.23</div>
+		</div>
+	);
+}
+
+export default Expense;

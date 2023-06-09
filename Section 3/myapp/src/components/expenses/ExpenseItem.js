1556fd3630c85cef25a8feb0fb1f6b837b35650c
lilo <lilo750@yahoo.com>
2023-06-09 21:03:08 +0200

    tweking in expenseItem file to make a button and when it clicked should change my title

--- Section 3/myapp/src/components/expenses/ExpenseItem.js
+++ Section 3/myapp/src/components/expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function Expense(props) {
+	const [title, setTitle] = useState(props.title);
+
+	function changeTitle() {
+		setTitle('title changed now');
+	}
+
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.price}</div>
 			</div>
+			<button onClick={changeTitle}>Click me to change title</button>
 		</Card>
 	);
 }

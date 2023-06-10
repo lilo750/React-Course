9701b9f1a180d539fb69d1777d230740943f7ee9
lilo <lilo750@yahoo.com>
2023-06-10 23:51:26 +0200

    save the inputs value as a state by creating a state to each input and add onChange prop to the inputs to save the date  after sumbit as a state

--- Section 3/myapp/src/components/NewExpense/ExpenseForm.js
+++ Section 3/myapp/src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
+	// now we make a state for every input
+	const [enteredTitle, setInteredTitle] = useState('');
+	const [enteredAmount, setInteredAmount] = useState('');
+	const [enteredDate, setInteredDate] = useState('');
+
+	function saveTitleHandler(event) {
+		// console.log('changed!');
+		// console.log(event.target.value);
+
+		setInteredTitle(event.target.value);
+	}
+
+	function saveAmountHandler(event) {
+		setInteredAmount(event.target.value);
+	}
+
+	function saveDateHandler(event) {
+		setInteredDate(event.target.value);
+	}
+
 	return (
 		<form>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' />
+					<input type='text' onChange={saveTitleHandler} />
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type='number' min='0.01' step='0.01' />
+					<input
+						type='number'
+						min='0.01'
+						step='0.01'
+						onChange={saveAmountHandler}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type='date' min='2018-01-01' max='2023-10-06' />
+					<input
+						type='date'
+						min='2018-01-01'
+						max='2023-10-06'
+						onChange={saveDateHandler}
+					/>
 				</div>
 				<div className='new-expense__actions'>
 					<button type='submit'>Add Expense</button>

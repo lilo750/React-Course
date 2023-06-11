6775ffcf5f84d0c61fbc6c1f480a87fe1f5e97ae
lilo <lilo750@yahoo.com>
2023-06-11 03:34:37 +0200

    i did the same prevous job but with the combined state approch (it's for test purpose

--- Section 3/myapp/src/components/NewExpense/ExpenseForm.js
+++ Section 3/myapp/src/components/NewExpense/ExpenseForm.js
@@ -4,60 +4,54 @@ import './ExpenseForm.css';
 function ExpenseForm() {
 	// now we make a state for every input (this called indvitaul state)
 
-	const [enteredTitle, setInteredTitle] = useState('');
+	/*
+  const [enteredTitle, setInteredTitle] = useState('');
 	const [enteredAmount, setInteredAmount] = useState('');
 	const [enteredDate, setInteredDate] = useState('');
+  */
 
-	/*
-  // All state in one state approch
+	// All state in one state approch
 
 	const [interedInput, setInteredInput] = useState({
 		enteredTitle: '',
 		enteredAmount: '',
 		enteredDate: '',
 	});
-   */
 
 	function saveTitleHandler(event) {
 		// console.log('changed!');
 		// console.log(event.target.value);
 
-		setInteredTitle(event.target.value);
+		// setInteredTitle(event.target.value);
 
-		/*
 		setInteredInput((prevState) => {
 			return {
 				...prevState,
 				enteredTitle: event.target.value,
 			};
 		});
-     */
 	}
 
 	function saveAmountHandler(event) {
-		setInteredAmount(event.target.value);
+		// setInteredAmount(event.target.value);
 
-		/*
 		setInteredInput((prevState) => {
 			return {
 				...prevState,
 				enteredAmount: event.target.value,
 			};
 		});
-     */
 	}
 
 	function saveDateHandler(event) {
-		setInteredDate(event.target.value);
+		// setInteredDate(event.target.value);
 
-		/*
 		setInteredInput((prevState) => {
 			return {
 				...prevState,
 				enteredDate: event.target.value,
 			};
 		});
-     */
 	}
 
 	function submitHandler(event) {
@@ -65,18 +59,31 @@ function ExpenseForm() {
 		event.preventDefault();
 
 		//save the inputed data in an object and log it
+
+		/*
 		const enteredData = {
 			title: enteredTitle,
 			cost: enteredAmount,
 			date: new Date(enteredDate),
 		};
+    */
 
-		console.log(enteredData);
+		// console.log(enteredData);
+		console.log(interedInput);
 
 		// clear the input fields after submit done and collected data in an object
-		setInteredTitle('');
-		setInteredAmount('');
-		setInteredDate('');
+
+		/*
+   setInteredTitle('');
+   setInteredAmount('');
+   setInteredDate('');
+   */
+
+		setInteredInput({
+			enteredTitle: '',
+			enteredAmount: '',
+			enteredDate: '',
+		});
 	}
 
 	return (
@@ -84,7 +91,11 @@ function ExpenseForm() {
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' onChange={saveTitleHandler} value={enteredTitle} />
+					<input
+						type='text'
+						onChange={saveTitleHandler}
+						value={interedInput.enteredTitle}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
@@ -93,7 +104,7 @@ function ExpenseForm() {
 						min='0.01'
 						step='0.01'
 						onChange={saveAmountHandler}
-						value={enteredAmount}
+						value={interedInput.enteredAmount}
 					/>
 				</div>
 				<div className='new-expense__control'>
@@ -103,7 +114,7 @@ function ExpenseForm() {
 						min='2018-01-01'
 						max='2023-10-06'
 						onChange={saveDateHandler}
-						value={enteredDate}
+						value={interedInput.enteredDate}
 					/>
 				</div>
 				<div className='new-expense__actions'>
